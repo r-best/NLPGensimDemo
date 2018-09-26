6eb0361639693958cae51f0e96bfce6268c51188
bobby <bobbyisbest3@gmail.com>
2018-09-26 13:55:54 -0400

    Creating extremely simple example

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 import os
+import multiprocessing
 from gensim import corpora, models
 from sklearn.decomposition import TruncatedSVD
 from sklearn.utils.extmath import randomized_svd
 from matplotlib import pyplot
 
-TRAINING_DIR = './training'
+TRAINING_DIR = './training_small/initial_corpus'
+ADAPT_DIR = './training_small/adapt'
 
 ## 
 # Takes in a variadic number of filepaths and
@@ -39,13 +41,13 @@ def preprocess(text):
     tokens = text.split(' ')
     tokens = [x for x in tokens if x is not ""]
 
-    frequency = {}
-    for token in tokens:
-        if token not in frequency:
-            frequency[token] = 1
-        else:
-            frequency[token] += 1
-    tokens = [x for x in tokens if frequency[x] > 1]
+    # frequency = {}
+    # for token in tokens:
+    #     if token not in frequency:
+    #         frequency[token] = 1
+    #     else:
+    #         frequency[token] += 1
+    # tokens = [x for x in tokens if frequency[x] > 1]
     
     return tokens
 
@@ -55,25 +57,24 @@ def preprocess(text):
 ##
 def main():
     # Read in training files and format them for use in the model
-    corpus = readFiles(TRAINING_DIR)
-    corpus = [preprocess(x) for x in corpus]
+    initial_corpus = readFiles(TRAINING_DIR)
+    initial_corpus = [preprocess(x) for x in initial_corpus]
+    adaptation_corpus = readFiles(ADAPT_DIR)
+    adaptation_corpus = [preprocess(x) for x in adaptation_corpus]
 
     # Instantiate model
-    model = models.Word2Vec(corpus, size=100, window=4, min_count=1, workers=6, alpha=0.025)
-    
-    # print(model)
-    # print(model['JERRY:'])
-    # print(model.wv['JERRY:'])
+    model = models.Word2Vec(initial_corpus, size=len(initial_corpus[0]), window=4, min_count=1, workers=multiprocessing.cpu_count(), alpha=0.025)
+    print(model)
+    print(model.wv['word1'])
+    # Printed weights of word1:
+    # [-0.01076334  0.00622735 -0.0402417   0.02217639  0.04611698  0.01181554  0.00560959  0.03880095 -0.04292683 -0.02537567]
 
-    # result = TruncatedSVD(n_components=2).fit_transform(model[model.wv.vocab])
-    U, Sigma, VT = randomized_svd(model[model.wv.vocab], n_components=2)
-    print("U", U)
-    print("SIGMA", Sigma)
-    print("VT", VT)
-    # pyplot.scatter(result[:, 0], result[:, 1])
-    # for i, word in enumerate(list(model.wv.vocab)):
-    #     pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
-    # pyplot.show()
+    # Add new data to model
+    model.train(adaptation_corpus, total_examples=model.corpus_count, epochs=model.iter)
+    print(model)
+    print(model.wv['word1'])
+    # Prints different from before (weights have changed):
+    # [-0.01776467  0.0080032  -0.03565837  0.02399305  0.05069579  0.00730562 0.00882716  0.03723516 -0.03821499 -0.03624377]
 
 
 if __name__ == '__main__':
